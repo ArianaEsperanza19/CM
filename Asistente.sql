2f02ec8171b05c3140e00a89220eac74c30e1686
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-01 18:34:03 -0400

    Agregar tablas nuevas a la DB, para registros e imagenes

--- Asistente.sql
+++ Asistente.sql
@@ -1,15 +1,16 @@
 CREATE DATABASE AsistenteSeguros;
 USE AsistenteSeguros;
-CREATE TABLE Titulares(
+
+CREATE TABLE Titulares (
     id_cliente INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50),
     segundo_nombre VARCHAR(50),
     primer_apellido VARCHAR(50),
     segundo_apellido VARCHAR(50),
     SSN VARCHAR(20),
-    alien_number VARCHAR(20), 
+    alien_number VARCHAR(20),
     genero VARCHAR(10),
-    fecha_nacimiento DATE,
+    fecha_nacimiento DATE DEFAULT NULL,
     direccion VARCHAR(100),
     ciudad VARCHAR(50),
     estado VARCHAR(50),
@@ -23,37 +24,52 @@ CREATE TABLE Titulares(
     notas VARCHAR(200)
 );
 
-CREATE TABLE Datos_Seguro(
+CREATE TABLE Datos_Seguro (
     id_cliente INT,
     policy_number VARCHAR(20),
     member_number VARCHAR(20),
     group_number VARCHAR(20),
     plan_seguro VARCHAR(20),
-    PRIMARY KEY (id_cliente), 
+    PRIMARY KEY (id_cliente),
     FOREIGN KEY (id_cliente) REFERENCES Titulares(id_cliente) ON DELETE CASCADE
 );
 
-CREATE TABLE Conyugues_Dependientes(
-    id_miembro_grupo INT AUTO_INCREMENT,
+CREATE TABLE Conyugues_Dependientes (
+    id_miembro_grupo INT PRIMARY KEY AUTO_INCREMENT,
     id_cliente INT,
     nombre VARCHAR(50),
     segundo_nombre VARCHAR(50),
     apellido VARCHAR(50),
     SSN VARCHAR(20),
-    alien_number VARCHAR(20), 
+    alien_number VARCHAR(20),
     genero VARCHAR(10),
-    fecha_nacimiento DATE,
+    fecha_nacimiento DATE DEFAULT NULL,
     en_poliza VARCHAR(20),
     estatus_migratorio BOOLEAN,
     pareja BOOLEAN,
-    PRIMARY KEY (id_miembro_grupo),
     FOREIGN KEY (id_cliente) REFERENCES Titulares(id_cliente) ON DELETE CASCADE
 );
 
-CREATE TABLE Cuentas(
+CREATE TABLE Cuentas (
     id_cliente INT,
     numero_cuenta VARCHAR(20),
     tipo_cuenta VARCHAR(20),
     PRIMARY KEY (id_cliente),
+    FOREIGN KEY (id_cliente) REFERENCES Titulares(id_cliente) ON DELETE CASCADE
+);
+
+CREATE TABLE Registros (
+    id_registro INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    fecha DATE DEFAULT (CURRENT_DATE),
+    descripcion VARCHAR(50),
+    FOREIGN KEY (id_cliente) REFERENCES Titulares(id_cliente) ON DELETE CASCADE
+);
+
+CREATE TABLE Img (
+    id_img INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    nombre VARCHAR(50),
+    imagen VARCHAR(250),
     FOREIGN KEY (id_cliente) REFERENCES Titulares(id_cliente) ON DELETE CASCADE
 );
