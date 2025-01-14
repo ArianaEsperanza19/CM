10ae732c226533b785ea16c96ef01e665dad3b49
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-14 19:56:53 -0400

    Preparar mensaje de error en caso de falla al conectarse a la base de datos

--- Config/DB.php
+++ Config/DB.php
@@ -1,11 +1,27 @@
 <?php
+$_SESSION['DB_name'] = "AsistenteSeguros";
+$_SESSION['DB_user'] = "root";
+$_SESSION['DB_pass'] = "";
 class DB{
     public static function Connect(){
-        $conexion = new PDO("mysql:host=localhost;dbname=AsistenteSeguros", "root", "");
-        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //$conexion = new mysqli("localhost", "root", "", "digitienda");
+    try {
+    $conexion = new PDO("mysql:host=localhost;dbname=".$_SESSION['DB_name'], $_SESSION['DB_user'], $_SESSION['DB_pass']);
+    }catch(PDOException $e){ {
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"Vistas/css/advertencia.css\">";
+    echo "<div id='advertencia' style='margin-top: 5%;'>";
+    if($e->getCode() == 1049){
+    echo "<p>Error de Conexión:<p>"."<p style='color:red;'>".$e->getMessage()."</p>";
+    echo "<p style='color:red;'>Base de datos no encontrada, verifique los datos del archivo 'Config/DB.php'</p>";
+    }else{
+    echo "<p>Error de Conexión:<p>"."<p style='color:red;'>".$e->getMessage()."</p>";
+    }
+    }
+    echo "</div>";
+    }
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$conexion = new mysqli("localhost", "root", "", "digitienda");
 		$conexion->query("SET NAMES 'utf8'");
-		
+
 		return $conexion;
     }
 }
