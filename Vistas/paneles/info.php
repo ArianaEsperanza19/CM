22877210e709370e85da93030402dd2b1777bb28
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-19 18:47:26 -0400

    Modificar panel de informacion del cliente para ver el estatus migratorio de cada miembro

--- Vistas/paneles/info.php
+++ Vistas/paneles/info.php
@@ -93,6 +93,11 @@ echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"Vistas/css/info.css\">";
     echo "<p><b>Alien Number:</b> $dato[alien_number]</p>";
     echo "<p><b>SSN:</b> $dato[SSN]</p>";
     echo "<p><b>Cobertura:</b> $dato[en_poliza]</p></div></div></div>";
+    if($dato['estatus_migratorio'] == 1){
+            echo "<p><b>Estatus migratorio: </b> <span>Si</span></p>";
+    }else{
+        echo "<p><b>Estatus migratorio: </b> <span>No</span></p>";
+    }
     }
 } else {
     // La consulta no ha encontrado nada
@@ -119,7 +124,13 @@ echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"Vistas/css/info.css\">";
     echo "<p><b>Alien Number:</b> $dato[alien_number]</p>";
     echo "<p><b>SSN:</b> $dato[SSN]</p>";
     echo "<p><b>Cobertura:</b> $dato[en_poliza]</p></div>
-    <p style='padding-left:10px;'><b> Relacion:</b> $dato[relacion]</p></div><hr><br>";
+    <p style='padding-left:10px;'><b>Relacion:</b> $dato[relacion]</p></div>";
+
+    if($dato['estatus_migratorio'] == 1){
+            echo "<p><b>Estatus migratorio: </b> <span>Si</span></p><hr><br>";
+    }else{
+        echo "<p><b>Estatus migratorio: </b> <span>No</span></p><hr><br>";
+    }
 }
     echo "<br><a class='boton' style:'margin-left:0px' href='?controller=Paneles&action=formularioDepende"."&id_cliente=$cliente&depende=0'>Agregar +</a><br><br>";
 ?>
