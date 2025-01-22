a9fea026f9838f20b2ca533ef4427e72103c7623
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-21 20:10:46 -0400

    Estilos aplicados a los formularios de imagenes

--- Vistas/paneles/pagina_img.php
+++ Vistas/paneles/pagina_img.php
@@ -1,11 +1,12 @@
 <?php
+
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"Vistas/css/pagina_img.css\">";
 // 0 mostrar
 // 1 agregar
 // 2 editar
 # Mostrar imagenes
 if (isset($_GET['add']) && isset($_GET['cliente']) && $_GET['add'] == 0) {
-echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=info&cliente=$cliente'>Volver</a>";
+    echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=info&cliente=$cliente'>Volver</a>";
     require_once "Modelos/DatosManager.php";
     $DB = new DatosManager(tabla: 'Img');
     $imagenes = $DB->Conseguir_Registro("WHERE id_cliente = $cliente");
@@ -14,14 +15,14 @@ echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=inf
         $img = $dato['imagen'];
     }
     if ($imagenes->rowCount() > 0) {
-    echo "<a class='boton' href='?controller=Paneles&action=pagina_img&cliente=$id&add=1&cliente=$cliente'>Agregar</a>";
-if(isset($_SESSION['flash'])){
-    $m = $_SESSION['flash'];
-    echo "<span style='color: red;margin-left: 10px'>$m</span>";
-    unset($_SESSION['flash']);
-}
-    echo "<h1>Documentos</h1>";
-    echo "<div class='contenedor'>";
+        echo "<a class='boton' href='?controller=Paneles&action=pagina_img&cliente=$id&add=1&cliente=$cliente'>Agregar</a>";
+        if (isset($_SESSION['flash'])) {
+            $m = $_SESSION['flash'];
+            echo "<span style='color: red;margin-left: 10px'>$m</span>";
+            unset($_SESSION['flash']);
+        }
+        echo "<h1>Documentos</h1>";
+        echo "<div class='contenedor'>";
         echo "<div id='datos'>";
         $DB = new DatosManager(tabla: 'Img');
         $imagenes = $DB->Conseguir_Registro("WHERE id_cliente = $cliente");
@@ -37,23 +38,24 @@ if(isset($_SESSION['flash'])){
             <a class='boton' href='?controller=Paneles&action=pagina_img&cliente=$cliente&add=2&img=$id'>Editar</a></div>
             </div></div>";
 
-        }}
+        }
+    }
     echo "</div>";
-        echo "</div>";
-    }else{
+    echo "</div>";
+} else {
 
-        $DB = new DatosManager(tabla: 'Img');
-        $imagenes = $DB->Conseguir_Registro("WHERE id_cliente = $cliente");
-if($imagenes->rowCount() == 0){
-echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=info&cliente=$cliente'>Volver</a>";
-}elseif($imagenes->rowCount() > 0){
-echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=pagina_img&cliente=$cliente&add=0'>Volver</a>";
+    $DB = new DatosManager(tabla: 'Img');
+    $imagenes = $DB->Conseguir_Registro("WHERE id_cliente = $cliente");
+    if ($imagenes->rowCount() == 0) {
+        echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=info&cliente=$cliente'>Volver</a>";
+    } elseif ($imagenes->rowCount() > 0) {
+        echo "<div id='contenedor'><a class='boton' href='?controller=Paneles&action=pagina_img&cliente=$cliente&add=0'>Volver</a>";
 
-}
-# Formulario para registrar imagen
-if (isset($_GET['add']) && isset($_GET['cliente']) && $_GET['add'] == 1) {
-    echo "
-    <div id='datos'>
+    }
+    # Formulario para registrar imagen
+    if (isset($_GET['add']) && isset($_GET['cliente']) && $_GET['add'] == 1) {
+        echo "
+    <div id='datos' class='formularioCaja'>
     <h1>Agregar Imagen</h1>
     <form action='?controller=Cliente&action=agregarImg' method='post' enctype='multipart/form-data'>
     <input type='hidden' name='id' value='" . $_GET['cliente'] . "'>
@@ -62,28 +64,30 @@ if (isset($_GET['add']) && isset($_GET['cliente']) && $_GET['add'] == 1) {
     <label for='descripcion'>Descripcion:</label><br>
     <input type='text' name='descripcion'><br>
     <label for='imagen'>Imagen:</label><br>
-    <input type='file' name='imagen' required><br>
-    <input type='submit' value='Enviar'>
+    <input id='imgField' type='file' name='imagen' required><br>
+    <input id='enviar' type='submit' value='Enviar'>
     </form>
     </div></div>";
-}
+    }
 
 
-# Editar imagen
-if (isset($_GET['add']) && isset($_GET['cliente']) && isset($_GET['img']) && $_GET['add'] == 2) {
-    require_once "Modelos/DatosManager.php";
-    $DB = new DatosManager(tabla: 'Img');
-    $img = $_GET['img'];
-    $imagenes = $DB->Conseguir_Registro("WHERE id_img = $img");
-    if ($imagenes->rowCount() > 0) {
-    foreach ($imagenes as $dato) {
-        $id = $dato['id_img'];
-        $Img = $dato['imagen'];
-        $nombre = $dato['nombre'];
-        $descripcion = $dato['descripcion'];
-    }}
-    echo "
+    # Editar imagen
+    if (isset($_GET['add']) && isset($_GET['cliente']) && isset($_GET['img']) && $_GET['add'] == 2) {
+        require_once "Modelos/DatosManager.php";
+        $DB = new DatosManager(tabla: 'Img');
+        $img = $_GET['img'];
+        $imagenes = $DB->Conseguir_Registro("WHERE id_img = $img");
+        if ($imagenes->rowCount() > 0) {
+            foreach ($imagenes as $dato) {
+                $id = $dato['id_img'];
+                $Img = $dato['imagen'];
+                $nombre = $dato['nombre'];
+                $descripcion = $dato['descripcion'];
+            }
+        }
+        echo "
     <div id='datos'>
+    <div id='datos' class='formularioCaja'>
     <h1>Agregar Imagen</h1>
     <form action='?controller=Cliente&action=editarImg' method='post' enctype='multipart/form-data'>
     <input type='hidden' name='id_img' value='" . $id . "'>
@@ -93,16 +97,16 @@ if (isset($_GET['add']) && isset($_GET['cliente']) && isset($_GET['img']) && $_G
     <label for='descripcion'>Descripcion:</label><br>
     <input value='$descripcion' type='text' name='descripcion'><br>
     <label for='imagen'>Imagen:</label><br>
-    <input type='file' name='imagen'><br>
-    <img class='miniatura' src='Vistas/img/$Img' alt='Imagen'>
-    <input type='submit' value='Enviar'>
+    <input id='imgField' type='file' name='imagen'><br>
+    <img class='miniatura' src='Vistas/img/$Img' alt='Imagen'><br>
+    <input id='enviar' style='margin-left: 0px' type='submit' value='Enviar'>
     </form>
-    </div></div>";
-}
-}
-    if($imagenes->rowCount() == 0){
-        echo "No se encontraron imágenes.";
+    </div></div></div>";
     }
+}
+if ($imagenes->rowCount() == 0) {
+    echo "No se encontraron imágenes.";
+}
 
 
 # Volver a la pagina del cliente
