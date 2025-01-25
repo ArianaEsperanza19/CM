8a1d9ebb24553851cfdc52069182b242813c7ed6
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-25 18:14:02 -0400

    Comentados los metodos del controlador Clientes

--- Controladores/ClienteController.php
+++ Controladores/ClienteController.php
@@ -10,6 +10,12 @@ class ClienteController
     public function VerUno()
     {
     }
+    /**
+     * Metodo que se encarga de registrar un nuevo cliente y redirigir segun sea necesario
+     * para registrar conyugue o dependientes.
+     *
+     * @return void
+     */
     public function Crear()
     {
         if (isset($_POST)) {
@@ -38,9 +44,14 @@ class ClienteController
             }
         }
     }
+    /**
+     * Metodo que se encarga de registrar un nuevo conyugue y redirigir segun sea necesario
+     * para registrar dependientes.
+     *
+     * @return void
+     */
     public function Agregar_Conyuge()
     {
-        # Verificar si se puede usar sin datosmanager
         if (isset($_POST)) {
             if ($_GET['id_cliente']) {
                 $id_cliente = $_GET['id_cliente'];
@@ -59,6 +70,18 @@ class ClienteController
         }
     }
 
+    /**
+     * Método que se encarga de registrar un nuevo dependiente para un cliente específico
+     * y redirige al formulario de dependientes.
+     *
+     * - Verifica si hay datos POST para procesar.
+     * - Obtiene el ID del cliente de la consulta GET.
+     * - Registra los datos del dependiente en la base de datos.
+     * - Redirige al usuario al formulario de dependientes para el cliente dado.
+     *
+     * @return void
+     */
+
     public function Agregar_Depende()
     {
         if (isset($_POST)) {
@@ -73,6 +96,18 @@ class ClienteController
             }
         }
     }
+    /**
+     * Método para editar información de un cliente, ya sea conyugue, dependiente o titular.
+     *
+     * - Verifica si hay datos POST para procesar.
+     * - Obtiene el ID del cliente de la consulta GET.
+     * - Si el cliente es un conyugue o dependiente, actualiza su información en la base de datos.
+     * - Si es un titular, actualiza la información correspondiente.
+     * - Redirige al usuario a la página de información del cliente con un token para evitar volver al formulario.
+     *
+     * @return void
+     */
+
     public function Editar()
     {
         if (isset($_POST)) {
@@ -105,6 +140,16 @@ class ClienteController
             }
         }
     }
+    /**
+     * Elimina un cliente o un miembro del grupo en la base de datos.
+     * 
+     * - Verifica si se ha proporcionado el ID del cliente en la consulta GET.
+     * - Si el cliente es un conyugue o dependiente, elimina su información en la base de datos.
+     * - Si es un titular, elimina la información correspondiente.
+     * - Redirige al usuario a la página de información del cliente o a la página de inicio si se ha eliminado un titular.
+     * 
+     * @return void
+     */
     public function Eliminar()
     {
         if (isset($_GET['cliente'])) {
@@ -144,6 +189,17 @@ class ClienteController
         }
 
     }
+    /**
+     * Registra o edita la informacion del seguro de un cliente en la base de datos.
+     *
+     * - Verifica si hay datos POST para procesar.
+     * - Obtiene el ID del cliente de la consulta GET.
+     * - Si se ha proporcionado el par;metro 'editar' y su valor es 1, actualiza la informacion del seguro.
+     * - De lo contrario, registra la informacion del seguro en la base de datos.
+     * - Redirige al usuario a la pagina de informacion del seguro correspondiente.
+     *
+     * @return void
+     */
     public function registrar_editar_seguro()
     {
         if (isset($_POST)) {
@@ -171,6 +227,18 @@ class ClienteController
 
         }
     }
+    /**
+     * Registra o edita la información bancaria de un cliente en la base de datos.
+     *
+     * - Verifica si hay datos POST para procesar.
+     * - Obtiene el ID del cliente de la consulta GET.
+     * - Si se ha proporcionado el parámetro 'editar' y su valor es 1, actualiza la información bancaria.
+     * - De lo contrario, registra la información bancaria en la base de datos.
+     * - Redirige al usuario a la página de información del banco correspondiente.
+     *
+     * @return void
+     */
+
     public function registrar_editar_banco()
     {
         if (isset($_POST)) {
@@ -196,6 +264,15 @@ class ClienteController
         }
     }
 
+    /**
+     * Elimina un seguro asociado a un cliente de la base de datos.
+     *
+     * - Verifica si el ID del cliente está presente en la consulta GET.
+     * - Llama al método eliminar del modelo Seguros para realizar la eliminación.
+     * - Redirige al usuario a la página de información del cliente si la eliminación es exitosa.
+     * - Muestra un mensaje de error en caso de fallo.
+     */
+
     public function eliminar_seguro()
     {
         if (isset($_GET)) {
@@ -211,6 +288,14 @@ class ClienteController
         }
     }
 
+    /**
+     * Elimina la información bancaria asociada a un cliente de la base de datos.
+     *
+     * - Verifica si el ID del cliente está presente en la consulta GET.
+     * - Llama al método eliminar del modelo Cuentas para realizar la eliminación.
+     * - Redirige al usuario a la página de información del cliente si la eliminación es exitosa.
+     * - Muestra un mensaje de error en caso de fallo.
+     */
     public function eliminar_banco()
     {
         if (isset($_GET)) {
@@ -226,6 +311,18 @@ class ClienteController
         }
     }
 
+    /**
+     * Agrega una imagen asociada a un cliente en la base de datos.
+     *
+     * - Verifica si el método de la petición es POST.
+     * - Obtiene los datos del formulario (id, nombre y descripción).
+     * - Verifica si se ha proporcionado una imagen.
+     * - Sube la imagen en la carpeta 'Vistas/img/' y la registra en la base de datos.
+     * - Redirige al usuario a la página de información de imagenes del cliente si la subida es exitosa.
+     * - Muestra un mensaje de error en caso de fallo.
+     *
+     * @return void
+     */
     public function agregarImg()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -251,6 +348,19 @@ class ClienteController
         }
 
     }
+    /**
+     * Elimina una imagen asociada a un cliente de la base de datos.
+     *
+     * - Verifica si el ID de la imagen y el ID del cliente están presentes en la consulta GET.
+     * - Obtiene la ruta de la imagen y la elimina del servidor.
+     * - Elimina la imagen de la base de datos.
+     * - Verifica si hay más imágenes asociadas al cliente en la base de datos.
+     * - Si no hay más imágenes, redirige al usuario a la página de información del cliente.
+     * - Si hay más imágenes, redirige al usuario a la página de información de imágenes del cliente.
+     * - Muestra un mensaje de error en caso de fallo.
+     *
+     * @return void
+     */
     public function eliminarImg()
     {
         if (isset($_GET)) {
@@ -285,6 +395,19 @@ class ClienteController
 
         }
     }
+
+/**
+ * Edita la información de una imagen asociada a un cliente.
+ *
+ * - Verifica si hay datos POST y archivos subidos.
+ * - Si no se ha subido una nueva imagen, redirige al usuario y muestra un mensaje de advertencia.
+ * - Si se ha subido una nueva imagen, elimina la imagen anterior del servidor y sube la nueva.
+ * - Actualiza la información de la imagen en la base de datos.
+ * - Redirige al usuario a la página de imágenes del cliente si la edición es exitosa.
+ * - Muestra un mensaje de error si ocurre una falla.
+ *
+ * @return void
+ */
 
     public function editarImg()
     {
@@ -322,6 +445,17 @@ class ClienteController
         }
     }
 
+    /**
+     * Agrega un registro a la base de datos.
+     *
+     * - Verifica si hay datos POST.
+     * - Obtiene el ID del cliente de la consulta POST.
+     * - Registra el registro en la base de datos.
+     * - Redirige al usuario a la página de información de registros del cliente.
+     * - Muestra un mensaje de error en caso de fallo.
+     *
+     * @return void
+     */
     public function agregarRegistro()
     {
         if (isset($_POST)) {
@@ -334,6 +468,16 @@ class ClienteController
         }
     }
 
+    /**
+     * Elimina un registro de la base de datos.
+     *
+     * - Verifica si se ha proporcionado el ID del registro y el ID del cliente en la consulta GET.
+     * - Llama al método eliminarRegistro del modelo Grupo para realizar la eliminación.
+     * - Redirige al usuario a la página de información del cliente si la eliminación es exitosa.
+     * - Muestra un mensaje de error en caso de fallo.
+     *
+     * @return void
+     */
     public function eliminarRegistro()
     {
         if (isset($_GET)) {
@@ -347,6 +491,18 @@ class ClienteController
             }
         }
     }
+    /**
+     * Edita un registro existente en la base de datos o muestra un formulario para editar.
+     *
+     * - Si se ha proporcionado el parámetro GET 'editar' con valor 1, se muestra un formulario para editar el registro
+     *   correspondiente.
+     * - Si se ha proporcionado una solicitud POST con los datos del registro a editar, llama al método actualizarRegistro
+     *   del modelo Grupo para realizar la actualización.
+     * - Redirige al usuario a la página de información de registros del cliente si la actualización es exitosa.
+     * - Muestra un mensaje de error en caso de fallo.
+     *
+     * @return void
+     */
     public function editarRegistro()
     {
         // Verifica si hay un parámetro GET
@@ -370,7 +526,17 @@ class ClienteController
         }
     }
 
-
+    /**
+     * Realiza una búsqueda en la base de datos de clientes.
+     *
+     * - Verifica si se ha proporcionado un término de búsqueda en la solicitud POST.
+     * - Llama al método Conseguir_Registro del modelo Titulares para realizar la búsqueda.
+     * - Si se encontraron coincidencias, muestra un listado con los resultados en la vista busqueda.php.
+     * - Si no se encontraron coincidencias, muestra un mensaje de error y redirige al usuario a la página principal.
+     * - Si no se ha proporcionado un término de búsqueda, redirige al usuario a la página principal.
+     *
+     * @return void
+     */
     public function buscar()
     {
 
