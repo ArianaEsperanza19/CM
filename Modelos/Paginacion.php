771b1998c2a0b271d0ef8566dfba9d3aef416d87
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-26 23:01:20 -0400

    Comentado y refactorizado el modelo Paginacion

--- Modelos/Paginacion.php
+++ Modelos/Paginacion.php
@@ -7,16 +7,19 @@ class Paginacion
 {
     private $db;
     private $titulares;
-    private $paginado = 6;
+    private $paginado = 1;
     private $num_paginas;
 
-    // Constructor: recibe la conexión a la base de datos
-    public function __construct($datos)
+    /**
+     * Constructor: recibe la conexión a la base de datos
+     *
+     * @param array $datos
+     */
+    public function __construct(array $datos)
     {
         $this->db = DB::Connect();
         $this->titulares = $datos;
-        $this->calcularNumeroPaginas();
-
+        $this->num_paginas = (int) ceil(count($datos) / $this->paginado);
     }
 
     // Método para comprobar el array de datos
@@ -28,82 +31,87 @@ class Paginacion
         return $this->titulares;
     }
 
-    public function calcularNumeroPaginas()
+    /**
+     * Calculate the number of pages based on the given array of id_titular
+     *
+     * @param array<int> $id_titular
+     * @return int
+     */
+    public function calcularNumeroPaginas(array $id_titular): int
     {
-        $id_titular = $this->titulares;
-        if (count($id_titular) % $this->paginado == 0) {
-            $this->num_paginas = count($id_titular) / $this->paginado;
-        } else {
-            while ((count($id_titular) % $this->paginado) != 0) {
-                array_push($id_titular, "*");
-            }
-            $this->num_paginas = count($id_titular) / $this->paginado;
-        }
-        return $this->num_paginas;
+        // if (count($id_titular) % $this->paginado == 0) {
+        //     return count($id_titular) / $this->paginado;
+        // } else {
+        //     while ((count($id_titular) % $this->paginado) != 0) {
+        //         array_push($id_titular, "*");
+        //     }
+        //     return count($id_titular) / $this->paginado;
+        // }
+        return (int) ceil(count($id_titular) / $this->paginado);
     }
     public function getNumPaginas()
     {
         return $this->num_paginas;
     }
 
-    // Método para paginar los datos
-    public function paginar($numPag)
+    /**
+     * Método para paginar los datos
+     *
+     * @param int $numPag
+     * @return array
+     */
+    public function paginar(int $numPag): array
     {
+        // $id_titular = $this->titulares;
+        // $posicion = 0;
+        // $envio = [];
 
+        // if ($numPag == 1 || $numPag == null) {
+        //     $titulares_filtrados = array_slice($id_titular, 0, $this->paginado);
+
+        //     foreach ($titulares_filtrados as $id) {
+        //         if ($id != "*") {
+        //             $sql = "SELECT * FROM Titulares WHERE id_cliente = :id;";
+        //             $titulares = $this->db->prepare($sql);
+        //             $titulares->execute(['id' => $id['id_cliente']]);
+        //             $titulares = $titulares->fetchAll();
+        //             foreach ($titulares as $i) {
+        //                 array_push($envio, $i);
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     $posicion = $this->paginado;
+        //     $posicion_inicial_registro = $posicion * $numPag - $this->paginado;
+        //     $titulares_filtrados = array_slice($id_titular, $posicion_inicial_registro, $this->paginado);
+
+        //     foreach ($titulares_filtrados as $id) {
+        //         if ($id != "*") {
+        //             $sql = "SELECT * FROM Titulares WHERE id_cliente = :id;";
+        //             $titulares = $this->db->prepare($sql);
+        //             $titulares->execute(['id' => $id[0]]);
+        //             $titulares = $titulares->fetchAll();
+        //             foreach ($titulares as $i) {
+        //                 array_push($envio, $i);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return $envio;
         $id_titular = $this->titulares;
-        $posicion = 0;
-        $envio = [];
+        $posicion_inicial_registro = ($numPag - 1) * $this->paginado;
 
-        if ($numPag == 1 || $numPag == null) {
-            $titulares_filtrados = array_slice($id_titular, 0, $this->paginado);
+        $in = implode(',', array_map(function ($id): string {
+            return $id == "*" ? 'NULL' : (string)$id['id_cliente'];
+        }, array_slice($id_titular, $posicion_inicial_registro, $this->paginado)));
 
-            foreach ($titulares_filtrados as $id) {
-                if ($id != "*") {
-                    $sql = "SELECT * FROM Titulares WHERE id_cliente = :id;";
-                    $titulares = $this->db->prepare($sql);
-                    $titulares->execute(['id' => $id['id_cliente']]);
-                    $titulares = $titulares->fetchAll();
-                    foreach ($titulares as $i) {
-                        array_push($envio, $i);
-                    }
-                }
-            }
-        } else {
-            $posicion = $this->paginado;
-            $posicion_inicial_registro = $posicion * $numPag - $this->paginado;
-            $titulares_filtrados = array_slice($id_titular, $posicion_inicial_registro, $this->paginado);
+        $sql = "SELECT * FROM Titulares WHERE id_cliente IN ($in)";
+        $titulares = $this->db->prepare($sql);
+        $titulares->execute();
 
-            foreach ($titulares_filtrados as $id) {
-                if ($id != "*") {
-                    $sql = "SELECT * FROM Titulares WHERE id_cliente = :id;";
-                    $titulares = $this->db->prepare($sql);
-                    $titulares->execute(['id' => $id[0]]);
-                    $titulares = $titulares->fetchAll();
-                    foreach ($titulares as $i) {
-                        array_push($envio, $i);
-                    }
-                }
-            }
-        }
-
-        return $envio;
+        return $titulares->fetchAll();
     }
 
 
 }
-
-// $db = new DB();
-// $db = $db->Connect();
-// $paginas = 0;
-// $id_titular = [];
-// $datos = $db->prepare("SELECT id_cliente FROM Titulares;");
-// $datos->execute();
-// $datos = $datos->fetchAll();
-//
-// $Paginacion = new Paginacion($datos);
-// echo $Paginacion->calcularNumeroPaginas();
-// $paginado = $Paginacion->paginar(2, $datos);
-// echo "<pre>";
-// print_r($paginado);
-// echo "</pre>";
-// echo $Paginacion->getNumPaginas();
